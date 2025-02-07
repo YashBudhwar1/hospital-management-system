bdc030fa4778d10a540771ea1945349e4d33303d
YashBudhwar1 <yashbudhwar1@gmail.com>
2025-02-07 18:56:28 -0500

    Initial commit

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react'
+
+const AuthLayout = ({children}:{children: React.ReactNode}) => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <div className='w-1/2 h-full flex items-center justify-center'>{children}</div>
+        <div className='hidden md:flex w-1/2 h-full relative'>
+            <Image
+              src="https://images.pexels.com/photos/4769130/pexels-photo-4769130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              width={1000}
+              height={1000}
+              alt="Doctors"
+              className="w-full h-full object-cover" 
+            />
+            <div className='absolute top-0 w-full h-full bg-black bg-opacity-40 z-10 flex flex-col items-center justify-center' >
+                <h1 className='text-3xl 2xl:text-5xl font-bold text-white'> Hospital Management System</h1>
+                <p className='text-blue-900 text-base'>Welcomes you</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default AuthLayout
